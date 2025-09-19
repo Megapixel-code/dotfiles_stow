984792f011cf7c7f86fbd9d8ee5ac5ed2d1dce1f
megapixel-code <chainemegapixel@gmail.com>
2025-09-19 10:24:27 +0200

    nvim config reformating

--- _install_files/4_nvim/README.md
+++ _install_files/4_nvim/README.md
@@ -15,7 +15,7 @@ sudo apt install gcc ninja-build gettext cmake curl build-essential git fonts-fo
     - On BSD, use `gmake` instead of `make`.
 4. `sudo make install`
     - Default install location is `/usr/local`
-    - On Debian/Ubuntu, instead of `sudo make install`, you can try `cd build && cpack -G DEB && sudo dpkg -i nvim-linux-<arch>.deb` (with `<arch>` either `x86_64` or `arm64`, run `uname-m`) to build DEB-package and install it. This helps ensure clean removal of installed files.
+    - On Debian/Ubuntu, instead of `sudo make install`, you can try `cd build && cpack -G DEB && sudo dpkg -i nvim-linux-<arch>.deb` (with `<arch>` either `x86_64` or `arm64`, run `uname -m`) to build DEB-package and install it. This helps ensure clean removal of installed files.
 
 # nvim kickstart
 
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -90,43 +90,35 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false -- true if you have a Nerd Font installed and selected in the terminal
 
 -- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+--  NOTE: See `:help vim.o` more options `:help option-list`
 
--- Make line numbers default
-vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.o.relativenumber = true
+vim.o.number = true -- see current line number, if !relativenumber see all lines numbers
+vim.o.relativenumber = true -- relative line numbers
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+vim.o.mouse = 'a' -- enable mouse mode
 
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = true
+vim.o.showmode = false -- show mode in the last line, unnessesary cause we aleready show the mode elsewhere
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
 end)
+--  TODO: continue here
 
--- Enable break indent
-vim.o.breakindent = true
 -- Indent formating :
 vim.g.python_recommended_style = 0
 vim.g.rust_recommended_style = 0
 vim.opt.tabstop = 4
-vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.expandtab = true
+
+vim.o.breakindent = true -- wrapped line will continue visually indented
 
 -- Save undo history
 vim.o.undofile = true
