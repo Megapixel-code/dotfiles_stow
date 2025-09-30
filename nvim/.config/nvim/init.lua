c786dc24c65dd43f4af2bb6e156d2e4c31c1263e
megapixel-code <chainemegapixel@gmail.com>
2025-09-30 23:36:43 +0200

    fix(nvim): code formatting in init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -135,14 +135,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
    end,
 })
 
--- autoformat on save
 vim.api.nvim_create_autocmd('LspAttach', {
    group = vim.api.nvim_create_augroup('my.lsp', {}),
    callback = function(args)
       local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      if not client then return end
+
       -- Auto-format ("lint") on save.
-      if not client:supports_method('textDocument/willSaveWaitUntil')
-          and client:supports_method('textDocument/formatting') then
+      if client:supports_method('textDocument/formatting') then
          vim.api.nvim_create_autocmd('BufWritePre', {
             group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
             buffer = args.buf,
