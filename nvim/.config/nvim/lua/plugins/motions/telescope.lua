11e232b45b2534f7fbaa583acf5629545367544f
megapixel-code <chainemegapixel@gmail.com>
2025-10-02 21:03:21 +0200

    feat(nvim/telescope): added telescope to nvim config

--- nvim/.config/nvim/lua/plugins/motions/telescope.lua
+++ nvim/.config/nvim/lua/plugins/motions/telescope.lua
@@ -1,15 +1,23 @@
-return 
-
-
-
-KeyRelease event, serial 36, synthetic NO, window 0x400001,
-root 0x27a, subw 0x0, time 18421526, (256,870), root:(2619,1571),
-state 0x4, keycode 105 (keysym 0xffe4, Control_R), same_screen YES,
-XLookupString gives 0 bytes:
-XFilterEvent returns: False
-
-
-xmodmap -e 'clear Control_R' 
-xmodmap -e 'keycode 37 = Alt_Gr' 
-
-
+return {
+   'nvim-telescope/telescope.nvim',
+   tag = '0.1.8',
+   dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+   },
+   config = function()
+      require('telescope').setup {
+         pickers = {
+            find_files = {
+               theme = "dropdown",
+            },
+         },
+      }
+      vim.keymap.set("n", "<leader>sf", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<leader>en", function()
+         require('telescope.builtin').find_files {
+            cwd = vim.fn.stdpath('config')
+         }
+      end)
+   end,
+}
