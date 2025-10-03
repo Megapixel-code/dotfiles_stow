6165d7c8096d01612a52b1a86a96a93294481ac5
megapixel-code <chainemegapixel@gmail.com>
2025-10-03 19:02:50 +0200

    feat(nvim/telescope): added search grep to nvim

--- nvim/.config/nvim/lua/plugins/motions/telescope.lua
+++ nvim/.config/nvim/lua/plugins/motions/telescope.lua
@@ -12,12 +12,24 @@ return {
                theme = "dropdown",
             },
          },
+         fzf = {},
       }
+
+      require('telescope').load_extension('fzf')
+
       vim.keymap.set("n", "<leader>sf", require('telescope.builtin').find_files)
-      vim.keymap.set("n", "<leader>en", function()
+      vim.keymap.set("n", "<leader>sh", require('telescope.builtin').help_tags)
+      vim.keymap.set("n", "<leader>sn", function()
          require('telescope.builtin').find_files {
             cwd = vim.fn.stdpath('config')
          }
       end)
+      vim.keymap.set("n", "<leader>sp", function()
+         require('telescope.builtin').find_files {
+            cwd = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy')
+         }
+      end)
+
+      require('config.telescope.multigrep').setup()
    end,
 }
