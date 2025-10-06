58ba0714190fd2175094ccf2c36597a07ebe7cc0
megapixel-code <chainemegapixel@gmail.com>
2025-10-07 00:23:34 +0200

    feat(nvim/motions): added new plugins to learn better motions.

---
+++ nvim/.config/nvim/lua/plugins/motions/flash.lua
@@ -0,0 +1,11 @@
+return {
+   "folke/flash.nvim",
+   event = "VeryLazy",
+   ---@type Flash.Config
+   opts = {
+   },
+   -- stylua: ignore
+   keys = {
+      { "zk", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+   },
+}
