4d7db91d379208a11fd1b90ca7e476632188c330
megapixel-code <chainemegapixel@gmail.com>
2025-10-10 11:34:07 +0200

    feat(zsh/aliases): better ls alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-alias ll='ls -Al'
+# du -sh {dir} show size of directory 
+
+# A = show hidden files
+# o = long listing format without group information
+# h = humain readable
+# p = indicator for dirs (/)
+# group-directories-first = pretty explicit
+# time-style=iso = show either Y-M-D or M-D H:m depending if current year or not
+# color=auto = add color
+alias ll='ls -A -o -h -p --group-directories-first --time-style=iso --color=auto'
+
 alias lt='tree -a'
