dceca7454fead66334f09540776419ce07a7d262
megapixel-code <chainemegapixel@gmail.com>
2025-10-13 15:20:52 +0200

    fix(nvim/indent): configuring indent

--- nvim/.config/nvim/lua/plugins/interface/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/interface/indent-blankline.lua
@@ -2,5 +2,28 @@
 return {
    "lukas-reineke/indent-blankline.nvim",
    main = "ibl",
-   opts = {},
+   opts = {
+      scope = {
+         show_start = false, -- shows an underline on the first line of the scope
+         show_end = false,   -- shows an underline on the last line of the scope
+      },
+      exclude = {
+         filetypes = {
+            "lspinfo",
+            "packer",
+            "checkhealth",
+            "help",
+            "man",
+            "gitcommit",
+            "TelescopePrompt",
+            "TelescopeResults",
+         },
+         buftypes = {
+            "terminal",
+            "nofile",
+            "quickfix",
+            "prompt",
+         },
+      },
+   },
 }
