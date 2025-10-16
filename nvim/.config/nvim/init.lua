6c8f993cdd41e50bc16ab904499242348b443738
megapixel-code <chainemegapixel@gmail.com>
2025-10-16 18:37:00 +0200

    feat(nvim/terminal): new floating terminal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -165,12 +165,29 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
    desc = "Highlight when yanking text",
+   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
    callback = function()
       vim.hl.on_yank()
    end,
 })
 
 
+-- Terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+   desc = "terminal",
+   group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+   callback = function()
+   end,
+})
+vim.keymap.set("n", "<leader>t", function()
+   vim.cmd.vnew()
+   vim.cmd.term()
+   vim.cmd.wincmd("J")
+   vim.api.nvim_win_set_height(0, 15)
+end, { desc = "open a Terminal" })
+
+
+-- [[ automatically change terminal theme on quit ]]
 local function autoscheme()
    if not vim.g.terminal_color_0 then
       return
