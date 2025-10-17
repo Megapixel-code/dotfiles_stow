d352a55186e9e7da6347266cac102c0df34fb75b
megapixel-code <chainemegapixel@gmail.com>
2025-10-17 16:26:21 +0200

    feat(nvim/startup): new startup screen

---
+++ nvim/.config/nvim/lua/plugins/interface/startup.lua
@@ -0,0 +1,271 @@
+local function drawing_computer() end
+
+local function drawing_montain()
+   return {
+      type = "text",
+      oldfiles_directory = false,
+      align = "center",
+      fold_section = false,
+      title = "Header",
+      margin = 5,
+      content = {
+         "    .                  .-.    .  _   *     _   .                  ",
+         "           *          /   \\     ((       _/ \\       *    .        ",
+         "         _    .   .--'\\/\\_ \\     `      /    \\  *    ___          ",
+         "     *  / \\_    _/ ^      \\/\\'__        /\\/\\  /\\  __/   \\ *       ",
+         "       /    \\  /    .'   _/  /  \\  *' /    \\/  \\/ .`'\\_/\\   .     ",
+         "  .   /\\/\\  /\\/ :' __  ^/  ^/    `--./.'  ^  `-.\\ _    _:\\ _      ",
+         "     /    \\/  \\  _/  \\-' __/.' ^ _   \\_   .'\\   _/ \\ .  __/ \\     ",
+         "   /\\  .-   `. \\/     \\ / -.   _/ \\ -. `_/   \\ /    `._/  ^  \\    ",
+         "  /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.  ",
+         "@/        `.  / /      `-.   /  .-'   / .   .'   \\    \\  \\  .-  \\%",
+         "@&8jgs@@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%",
+         "@88:::&(&8&&8:::::%&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::",
+         "`::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8:::::' ",
+         " `::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.'  ",
+      },
+      highlight = "Statement",
+      default_color = "",
+      oldfiles_amount = 0,
+   }
+end
+
+local function drawing_periodic_table()
+   return {
+      type = "text",
+      oldfiles_directory = false,
+      align = "center",
+      fold_section = false,
+      title = "Header",
+      margin = 5,
+      content = {
+         "  -----                                                               -----",
+         "1 | H |                                                               |He |",
+         "  |---+----                                       --------------------+---|",
+         "2 |Li |Be |                                       | B | C | N | O | F |Ne |",
+         "  |---+---|                                       |---+---+---+---+---+---|",
+         "3 |Na |Mg |3B  4B  5B  6B  7B |    8B     |1B  2B |Al |Si | P | S |Cl |Ar |",
+         "  |---+---+---------------------------------------+---+---+---+---+---+---|",
+         "4 | K |Ca |Sc |Ti | V |Cr |Mn |Fe |Co |Ni |Cu |Zn |Ga |Ge |As |Se |Br |Kr |",
+         "  |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|",
+         "5 |Rb |Sr | Y |Zr |Nb |Mo |Tc |Ru |Rh |Pd |Ag |Cd |In |Sn |Sb |Te | I |Xe |",
+         "  |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|",
+         "6 |Cs |Ba |LAN|Hf |Ta | W |Re |Os |Ir |Pt |Au |Hg |Tl |Pb |Bi |Po |At |Rn |",
+         "  |---+---+---+------------------------------------------------------------",
+         "7 |Fr |Ra |ACT|",
+         "  -------------",
+         "              -------------------------------------------------------------",
+         "   Lanthanide |La |Ce |Pr |Nd |Pm |Sm |Eu |Gd |Tb |Dy |Ho |Er |Tm |Yb |Lu |",
+         "              |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|",
+         "   Actinide   |Ac |Th |Pa | U |Np |Pu |Am |Cm |Bk |Cf |Es |Fm |Md |No |Lw |",
+         "              -------------------------------------------------------------",
+      },
+      highlight = "Statement",
+      default_color = "",
+      oldfiles_amount = 0,
+   }
+end
+
+local function drawing_fractal_maze()
+   return {
+      type = "text",
+      oldfiles_directory = false,
+      align = "center",
+      fold_section = false,
+      title = "Header",
+      margin = 5,
+      content = {
+         " _   ___   ___   ___   ___   _ ",
+         " _| |_  |_|  _| |_  |_|  _| |_ ",
+         "|  _  |  _  |_   _|  _  |  _  |",
+         "|_| |_| | |___| |___| | |_| |_|",
+         " _   _  |  ___   ___  |  _   _ ",
+         "| |_| | |_|  _| |_  |_| | |_| |",
+         "|_   _|  _  |_   _|  _  |_   _|",
+         " _| |___| |___| |___| |___| |_ ",
+         "|  ___   ___   _   ___   ___  |",
+         "|_|  _| |_  |_| |_|  _| |_  |_|",
+         " _  |_   _|  _   _  |_   _|  _ ",
+         "| |___| |___| | | |___| |___| |",
+         "|_   _____   _| |_   _____   _|",
+         " _| |_   _| |_   _| |_   _| |_ ",
+         "|  _  | |  _  | |  _  | |  _  |",
+         "|_| |_| |_| |_| |_| |_| |_| |_|",
+      },
+      highlight = "Statement",
+      default_color = "",
+      oldfiles_amount = 0,
+   }
+end
+
+local function drawing_desert()
+   return {
+      type = "text",
+      oldfiles_directory = false,
+      align = "center",
+      fold_section = false,
+      title = "Header",
+      margin = 5,
+      content = {
+         "           ,,                               .-.         ",
+         "          || |                               ) )        ",
+         "          || |   ,                          '-'         ",
+         "          || |  | |                                     ",
+         "          || '--' |                                     ",
+         "    ,,    || .----'                                     ",
+         "   || |   || |                                          ",
+         "   |  '---'| |                                          ",
+         "   '------.| |                                  _____   ",
+         "   ((_))  || |      (  _                       / /|\\ \\  ",
+         "   (o o)  || |      ))(\"),                    | | | | | ",
+         "____\\_/___||_|_____((__^_))____________________\\_\\|/_/_ ",
+      },
+      highlight = "Statement",
+      default_color = "",
+      oldfiles_amount = 0,
+   }
+end
+
+local function content()
+   return {
+      type = "mapping",
+      oldfiles_directory = false,
+      align = "center",
+      fold_section = false,
+      title = "Basic Commands",
+      margin = 5,
+      content = {
+         { " Search File", "Telescope find_files", "<leader>sf" },
+         { "󰍉 Search Grep", "Telescope live_grep", "<leader>sg" },
+         { " Open Terminal", "FloatingTerminal", "<leader>t" },
+         { " Search Themes", "Themery", "<leader>st" },
+         { "󰋖 Search Help", "Telescope help_tags", "<leader>sh" },
+
+      },
+      highlight = "String",
+      default_color = "",
+      oldfiles_amount = 0,
+   }
+end
+
+local function random_drawing()
+   math.randomseed(os.time())
+   local drawings = {
+      drawing_periodic_table(),
+      drawing_montain(),
+      drawing_fractal_maze(),
+      drawing_desert(),
+   }
+   return drawings[math.random(#drawings)]
+end
+
+return {
+   "startup-nvim/startup.nvim",
+   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+   config = function()
+      require "startup".setup({
+         header = random_drawing(), -- drawing section
+         body = content(),          -- main section
+         options = {
+            mapping_keys = true,    -- display mapping
+
+            -- if < 1 fraction of screen width
+            -- if > 1 numbers of column
+            cursor_column = 0.5,
+
+            after = function() -- function that gets executed at the end
+               -- <lua commands>
+            end,
+            empty_lines_between_mappings = true, -- add an empty line between mapping/commands
+            disable_statuslines = true,          -- disable status-, buffer- and tablines
+            paddings = { 1, 4 },                 -- amount of empty lines before each section (must be equal to amount of sections)
+         },
+         mappings = {
+            execute_command = "<CR>",
+            open_file = "o",
+            open_file_split = "<c-o>",
+            open_section = "<TAB>",
+            open_help = "?",
+         },
+         colors = {
+            background = "#1f2227",
+            folded_section = "#56b6c2", -- the color of folded sections
+            -- this can also be changed with the `StartupFoldedSection` highlight group
+         },
+         parts = { "header", "body" } -- all sections in order
+      })
+   end
+}
+
+--[[
+
+=====================================================================
+=====================================================================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||        Nvim        ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||                    ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ fuck the \     ========
+========       /:::========|  |==hjkl==:::\  \   mouse  \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+
+    .                  .-.    .  _   *     _   .
+           *          /   \     ((       _/ \       *    .
+         _    .   .--'\/\_ \     `      /    \  *    ___
+     *  / \_    _/ ^      \/\'__        /\/\  /\  __/   \ *
+       /    \  /    .'   _/  /  \  *' /    \/  \/ .`'\_/\   .
+  .   /\/\  /\/ :' __  ^/  ^/    `--./.'  ^  `-.\ _    _:\ _
+     /    \/  \  _/  \-' __/.' ^ _   \_   .'\   _/ \ .  __/ \
+   /\  .-   `. \/     \ / -.   _/ \ -. `_/   \ /    `._/  ^  \
+  /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.
+@/        `.  / /      `-.   /  .-'   / .   .'   \    \  \  .-  \%
+@&8jgs@@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%
+@88:::&(&8&&8:::::%&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::
+`::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8:::::'
+ `::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.'
+
+  -----                                                               -----
+1 | H |                                                               |He |
+  |---+----                                       --------------------+---|
+2 |Li |Be |                                       | B | C | N | O | F |Ne |
+  |---+---|                                       |---+---+---+---+---+---|
+3 |Na |Mg |3B  4B  5B  6B  7B |    8B     |1B  2B |Al |Si | P | S |Cl |Ar |
+  |---+---+---------------------------------------+---+---+---+---+---+---|
+4 | K |Ca |Sc |Ti | V |Cr |Mn |Fe |Co |Ni |Cu |Zn |Ga |Ge |As |Se |Br |Kr |
+  |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|
+5 |Rb |Sr | Y |Zr |Nb |Mo |Tc |Ru |Rh |Pd |Ag |Cd |In |Sn |Sb |Te | I |Xe |
+  |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|
+6 |Cs |Ba |LAN|Hf |Ta | W |Re |Os |Ir |Pt |Au |Hg |Tl |Pb |Bi |Po |At |Rn |
+  |---+---+---+------------------------------------------------------------
+7 |Fr |Ra |ACT|
+  -------------
+              -------------------------------------------------------------
+   Lanthanide |La |Ce |Pr |Nd |Pm |Sm |Eu |Gd |Tb |Dy |Ho |Er |Tm |Yb |Lu |
+              |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|
+   Actinide   |Ac |Th |Pa | U |Np |Pu |Am |Cm |Bk |Cf |Es |Fm |Md |No |Lw |
+              -------------------------------------------------------------
+
+           ,,                               .-.
+          || |                               ) )
+          || |   ,                          '-'
+          || |  | |
+          || '--' |
+    ,,    || .----'
+   || |   || |
+   |  '---'| |
+   '------.| |                                  _____
+   ((_))  || |      (  _                       / /|\ \
+   (o o)  || |      ))("),                    | | | | |
+____\_/___||_|_____((__^_))____________________\_\|/_/__ldb
+
+--]]
