a30bd9943310a4b3e07347f5e57ab456e5073396
megapixel-code <chainemegapixel@gmail.com>
2025-11-01 16:04:33 +0100

    refactor(nvim/init): moved around settings, moved comments.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,67 +1,53 @@
--- ~~~~~~[[ My nvim config :0 ]]~~~~~~
+-- ~~~~~~ [[ My nvim config :0 ]] ~~~~~~
 
-
--- Set <space> as the leader key
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = " "      -- sets space as the leader key
+vim.g.maplocalleader = " " -- must be before loading plugins
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
 vim.schedule(function()
    vim.opt.clipboard = "unnamedplus"
 end)
 
--- [[ Setting options ]]
---  NOTE: See `:help vim.o` more options `:help option-list`
+-- ~~~ [[ Setting options ]] ~~~
+
+-- [[ interface ]]
+-- sets how neovim will display certain whitespace characters in the editor
+-- notice listchars is set using `vim.opt` instead of `vim.o`, used to interract with tables
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.o.number = true         -- see current line number, if !relativenumber see all lines numbers
 vim.o.relativenumber = true -- relative line numbers
 vim.o.cursorline = true     -- highlight the cursor line
 vim.o.scrolloff = 10        -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.showmode = false      -- show mode in the last line, unnessesary cause we aleready show the mode elsewhere
+vim.o.showmode = false      -- show mode in the last line, disabled because we have lualine
 vim.o.signcolumn = "yes"    -- Keep signcolumn on by default
 vim.g.have_nerd_font = true -- true if you have a Nerd Font installed and selected in the terminal
 vim.o.termguicolors = true  -- enable true color support
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables
-vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
-vim.o.mouse = "a" -- enable mouse mode
-
--- Indent formating :
+-- [[ formating ]]
 -- vim.g.python_recommended_style = 0
 -- vim.g.rust_recommended_style = 0
-vim.opt.tabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.tabstop = 3        -- numbers of spaces that a <Tab> count for
+vim.opt.shiftwidth = 0     -- number of spaces for each step of indent, 0 = take tabstop value
 vim.opt.smartindent = true -- auto indent new lines eg : after '{'
 vim.opt.expandtab = true   -- in insert mode adds the correct amount of spaces when you tab
 vim.o.wrap = true          -- allow line to wrap on the right
 vim.o.linebreak = true     -- wrap by word instead of letter
-vim.o.breakindent = true   -- wrapped lines will be indent
+vim.o.breakindent = true   -- wrapped lines will be indented
 
+-- [[ utilities ]]
+vim.o.mouse = "a"          -- enable mouse mode
 vim.o.undofile = true      -- Save undo history
 vim.o.confirm = true       -- ask confirmation when you quit while unsaved
-
--- Case-insensitive searching UNLESS or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Decrease update time
-vim.o.updatetime = 250
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 500
-
--- Configure how new splits should be opened
-vim.o.splitbelow = true
-vim.o.splitright = true
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = "split"
+vim.o.ignorecase = true    -- case-insensitive searching
+vim.o.smartcase = true     -- removes case-insensitive searching if one or more capital letter
+vim.o.updatetime = 250     -- time with nothing typed before writing swap to disk (ex crash recovery)
+vim.o.timeoutlen = 500     -- mapped sequence wait time
+vim.o.inccommand = "split" -- Preview substitutions live, as you type!
+vim.o.splitbelow = true    -- new splits open on the bottom
+vim.o.splitright = true    -- new splits open on the right
 
 -- [[ Autocommands ]]
 --  See `:help lua-guide-autocommands`
