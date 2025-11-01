482b7fc793b118124202e3469bb07f65a3c82835
megapixel-code <chainemegapixel@gmail.com>
2025-11-01 16:30:25 +0100

    refactor(nvim/keymaps): very light refactoring, changed '' to ""

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>", { desc = "Rel[O]ad f
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clears higlighting of search" })
 
 -- Quickfix
-vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
-vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
