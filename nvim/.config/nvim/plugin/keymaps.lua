b8994e06a7cfe208817ce805d1b7e384e7f69765
megapixel-code <chainemegapixel@gmail.com>
2025-11-02 18:27:10 +0100

    feat(nvim/terminal): removed floating_terminal and created terminal now able to toggle a terminal

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -26,7 +26,7 @@ vim.keymap.set("i", "\"", "\"\"<left>", { desc = "autoclose double quote" })
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
---vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
---
+++ nvim/.config/nvim/plugin/terminal.lua
@@ -0,0 +1,35 @@
+local term_state = {
+   win = -1,
+   buf = -1,
+}
+
+local create_win = function()
+   local buf = nil
+   if vim.api.nvim_buf_is_valid(term_state.buf) then
+      buf = term_state.buf
+   else
+      buf = vim.api.nvim_create_buf(true, true)
+      term_state.buf = buf
+   end
+
+   term_state.win = vim.api.nvim_open_win(buf, true, {
+      split = "below",
+      win = -1,
+      height = 15,
+   })
+
+   if vim.bo[term_state.buf].buftype ~= "terminal" then
+      vim.cmd.term()
+   end
+end
+
+local toggle_terminal = function()
+   if vim.api.nvim_win_is_valid(term_state.win) then
+      vim.api.nvim_win_hide(term_state.win)
+   else
+      create_win()
+   end
+end
+
+
+vim.keymap.set("n", "<leader>t", toggle_terminal)
