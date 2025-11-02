dafed34022d47d4252d5cfb05869f72bc2e9f043
megapixel-code <chainemegapixel@gmail.com>
2025-11-02 20:12:12 +0100

    feat(zsh/aliases): yazi now changes directories on quit

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -16,3 +16,11 @@ ll() {
 
 alias lt='tree -a'
 alias python=python3
+
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
