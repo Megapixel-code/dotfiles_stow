67610efbf564482d806414b26c908c6ff170542c
megapixel-code <chainemegapixel@gmail.com>
2025-11-03 01:18:54 +0100

    refactor(nvim/keymaps): removed comments, moved K hover keymap here

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -14,26 +14,21 @@ vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Auto close brackets and stuff
-vim.keymap.set("i", "{", "{}<left>", { desc = "autoclose brackets" })
-vim.keymap.set("i", "[", "[]<left>", { desc = "autoclose square brackets" })
-vim.keymap.set("i", "(", "()<left>", { desc = "autoclose parenteses" })
-vim.keymap.set("i", "'", "''<left>", { desc = "autoclose single quote" })
-vim.keymap.set("i", "\"", "\"\"<left>", { desc = "autoclose double quote" })
+vim.keymap.set("i", "{", "{}<left>", { desc = "Autoclose brackets" })
+vim.keymap.set("i", "[", "[]<left>", { desc = "Autoclose square brackets" })
+vim.keymap.set("i", "(", "()<left>", { desc = "Autoclose parenteses" })
+vim.keymap.set("i", "'", "''<left>", { desc = "Autoclose single quote" })
+vim.keymap.set("i", '"', '""<left>', { desc = "Autoclose double quote" })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+-- lsp
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Information" })
 
 --  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
