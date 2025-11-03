a42ae640d494791ed89f65631df61337b9879ab9
megapixel-code <chainemegapixel@gmail.com>
2025-11-03 11:04:41 +0100

    feat(nvim/conform): a bit more config and added one comment

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -2,7 +2,9 @@ return {
    "stevearc/conform.nvim",
    opts = {
       formatters_by_ft = {
-         lua = { "emmylua-codeformat" },
+         lua = { "emmylua-codeformat" }, -- lua formatter, look in .editorconfig
       },
+      notify_on_error = true,
+      notify_no_formatters = true,
    },
 }
