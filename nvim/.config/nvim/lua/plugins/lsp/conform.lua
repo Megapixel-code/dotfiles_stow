51535d899ba24b2b3c0b7efb0d36e3f25e6f62af
Megapixel-code <chainemegapixel@gmail.com>
2025-11-05 11:53:28 +0100

    refactor(nvim/conform): moved around formatters to make it consistent

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -2,10 +2,10 @@ return {
    "stevearc/conform.nvim",
    opts = {
       formatters_by_ft = {
-         lua = { "emmylua-codeformat" }, -- lua formatter, look in .editorconfig
-
-         sh = { "shfmt" },               -- bash formatter, look in .editorconfig
+         sh = { "shfmt" }, -- bash formatter, look in .editorconfig
          zsh = { "shfmt" },
+
+         lua = { "emmylua-codeformat" }, -- lua formatter, look in .editorconfig
       },
       notify_on_error = true,
       notify_no_formatters = true,
