696992235bdd5c8543c0f643b3ceb19d15f6d4c0
megapixel-code <chainemegapixel@gmail.com>
2025-11-06 13:11:02 +0100

    fix(nvim/color-picker): text colors now work with uppercases

--- nvim/.config/nvim/lua/plugins/interface/color-picker.lua
+++ nvim/.config/nvim/lua/plugins/interface/color-picker.lua
@@ -33,11 +33,17 @@ return {
 
             ccc.picker.custom_entries( { -- custom pickers when a word apears
                red = "#FF0000",
+               RED = "#FF0000",
                green = "#00FF00",
+               GREEN = "#00FF00",
                blue = "#0000FF",
+               BLUE = "#0000FF",
                yellow = "#FFFF00",
+               YELLOW = "#FFFF00",
                cyan = "#00FFFF",
+               CYAN = "#00FFFF",
                purple = "#FF00FF",
+               PURPLE = "#FF00FF",
             } ),
          },
       } )
