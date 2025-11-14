189d2021710b0ed84ad4e4edcc12d8dc94eb5a4a
megapixel-code <chainemegapixel@gmail.com>
2025-11-14 17:26:38 +0100

    fix(nvim/tabout): removed tabout in typst files

--- nvim/.config/nvim/lua/plugins/motions/tabout.lua
+++ nvim/.config/nvim/lua/plugins/motions/tabout.lua
@@ -30,7 +30,7 @@ return {
             { open = "{", close = "}" },
          },
          ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-         exclude = {}, -- tabout will ignore these filetypes
+         exclude = { "typst" }, -- tabout will ignore these filetypes
       } )
    end,
 }
