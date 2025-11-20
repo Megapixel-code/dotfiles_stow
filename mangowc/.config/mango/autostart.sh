f0439635d4107f1404862c55c59b5e4bef477d51
Megapixel-code <chainemegapixel@gmail.com>
2025-11-20 16:36:57 +0100

    feat(mangowc): made progress in the mango config

---
+++ mangowc/.config/mango/autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# cmd > /dev/null 2>&1 : used to discard output of cmd
+
+# bar
+waybar > /dev/null 2>&1 &
+
+# blue light filter
+sunsetr > /dev/null 2>&1 &
+
+# background
+
+# metropolis :
+# swaybg -o "*" -i "$XDG_CONFIG_HOME/mango/backgrounds/metropolis.png" -m "fill" > /dev/null 2>&1 &
+# swaybg -o "DP-1" -i "$XDG_CONFIG_HOME/mango/backgrounds/metropolis_horizontal.png" -m "fill" > /dev/null 2>&1 &
+# swaybg -o "HDMI-A-1" -i "$XDG_CONFIG_HOME/mango/backgrounds/metropolis_vertical.png" -m "fill" > /dev/null 2>&1 &
+
+# nixos
+swaybg -o "*" -i "$XDG_CONFIG_HOME/mango/backgrounds/nix-d-nord-purple.svg" -m "fill" > /dev/null 2>&1 &
