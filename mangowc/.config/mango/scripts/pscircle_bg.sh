188a8c9ec13b6c67c912dd491f4d57890f6431a0
Megapixel-code <chainemegapixel@gmail.com>
2025-12-01 14:22:59 +0100

    feat(mango/scripts): changed pscircle_bg; colors, animation

--- mangowc/.config/mango/scripts/pscircle_bg.sh
+++ mangowc/.config/mango/scripts/pscircle_bg.sh
@@ -7,17 +7,19 @@
 
 color_light="CF4794" #CF4794
 color_dark="1B1B3A" #1B1B3A
+color_mid="693668" #693668
+
 
 # https://gitlab.com/mildlyparallel/pscircle
 pscircle \
    --background-color="$color_light" \
-	--link-color-min="$color_dark" \
-	--link-color-max="$color_dark" \
+	--link-color-min="$color_mid" \
+	--link-color-max="$color_mid" \
 	--dot-color-min="$color_dark" \
 	--dot-color-max="$color_dark" \
 	--dot-border-color-min="$color_dark" \
-	--dot-border-color-max="$color_dark" \
-	--tree-font-color=000000CC \
+	--dot-border-color-max="$color_mid" \
+	--tree-font-color="$color_dark" \
 \
 	--tree-font-size=12 \
 	--tree-font-face="BlexMono Nerd Font Mono" \
@@ -41,7 +43,7 @@ pscircle \
 swww img -o "HDMI-A-1" "$XDG_CONFIG_HOME/mango/backgrounds/nix_horizontal.png"
 
 while inotifywait -e close_write "$XDG_CONFIG_HOME/mango/backgrounds/pscircle_out.png"; do
-   swww img -o "DP-1" "$XDG_CONFIG_HOME/mango/backgrounds/pscircle_out.png"
+   swww img -o "DP-1" "$XDG_CONFIG_HOME/mango/backgrounds/pscircle_out.png" --transition-type "grow" --transition-pos "center"
 done
 
 
