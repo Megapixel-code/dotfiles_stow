19a249a4fd573d4bd9c20059d849db59b27b4b62
Megapixel-code <chainemegapixel@gmail.com>
2025-12-08 20:27:08 +0100

    feat(nvim/conform): removed useless formatters, refactor

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -2,17 +2,25 @@ return {
    "stevearc/conform.nvim",
    opts = {
       formatters_by_ft = {
-         sh = { "shfmt" }, -- bash, look in .editorconfig
-         zsh = { "shfmt" },
+         sh = { "shfmt" },       -- bash, look in .editorconfig
+         zsh = { "shfmt" },      -- ^
 
-         lua = { "emmylua-codeformat" }, -- lua, look in .editorconfig
+         css = { "prettierd" },  -- angular, css, flow, graphql, html, json, jsx, javascript, less, markdown, scss, typescript, vue, yaml formatter
 
-         html = { "prettierd" },         -- angular, css, flow, graphql, html, json, jsx, javascript, less, markdown, scss, typescript, vue, yaml formatter
-         css = { "prettierd" },
-
-         nix = { "nixfmt" }, -- nix
+         typst = { "typstyle" }, -- typst
+         nix = { "nixfmt" },     -- nix
       },
+      formatters = {
+         typstyle = {
+            inherit = false,
+            stdin = false,
+            command = "typstyle",
+            args = { "-i", "@%" },
+         },
+      },
+
       notify_on_error = true,
       notify_no_formatters = true,
+      log_level = vim.log.levels.TRACE,
    },
 }
