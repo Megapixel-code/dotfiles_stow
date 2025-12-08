d7f40a58ed447b0504d4f98fbbc0e56b4c93fdfb
Megapixel-code <chainemegapixel@gmail.com>
2025-12-08 19:34:45 +0100

    feat(nvim/init): added spellcheck to Neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,8 +6,8 @@ vim.g.maplocalleader = " " -- must be before loading plugins
 -- ~~~ [[ SETTING OPTIONS ]] ~~~
 
 -- [[ interface ]]
--- sets how neovim will display certain whitespace characters in the editor
--- notice listchars is set using `vim.opt` instead of `vim.o`, used to interract with tables
+-- sets how Neovim will display certain whitespace characters in the editor
+-- notice listchars is set using `vim.opt` instead of `vim.o`, used to interact with tables
 vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
@@ -20,7 +20,7 @@ vim.o.signcolumn = "yes"    -- keep signcolumn on by default
 vim.g.have_nerd_font = true -- true if you have a Nerd Font installed and selected in the terminal
 vim.o.termguicolors = true  -- enable true color support
 
--- [[ formating ]]
+-- [[ formatting ]]
 -- vim.g.python_recommended_style = 0
 -- vim.g.rust_recommended_style = 0
 vim.opt.tabstop = 3        -- numbers of spaces that a <Tab> count for
@@ -32,23 +32,25 @@ vim.o.linebreak = true     -- wrap by word instead of letter
 vim.o.breakindent = true   -- wrapped lines will be indented
 
 -- [[ utilities ]]
-vim.o.mouse = "a"          -- enable mouse mode
-vim.o.undofile = true      -- save undo history
-vim.o.confirm = true       -- ask confirmation when you quit while unsaved
-vim.o.ignorecase = true    -- case-insensitive searching
-vim.o.smartcase = true     -- removes case-insensitive searching if one or more capital letter
-vim.o.updatetime = 250     -- time with nothing typed before writing swap to disk (ex crash recovery)
-vim.o.timeoutlen = 500     -- mapped sequence wait time
-vim.o.inccommand = "split" -- preview substitutions live, as you type!
-vim.o.splitbelow = true    -- new splits open on the bottom
-vim.o.splitright = true    -- new splits open on the right
+vim.o.mouse = "a"              -- enable mouse mode
+vim.o.undofile = true          -- save undo history
+vim.o.confirm = true           -- ask confirmation when you quit while unsaved
+vim.o.ignorecase = true        -- case-insensitive searching
+vim.o.smartcase = true         -- removes case-insensitive searching if one or more capital letter
+vim.o.updatetime = 250         -- time with nothing typed before writing swap to disk (ex crash recovery)
+vim.o.timeoutlen = 500         -- mapped sequence wait time
+vim.o.inccommand = "split"     -- preview substitutions live, as you type!
+vim.o.splitbelow = true        -- new splits open on the bottom
+vim.o.splitright = true        -- new splits open on the right
+vim.cmd("set spelllang=en_us") -- choose English spelling
+vim.cmd("set spell")           -- enable spelling
 
 -- ~~~ [[ OTHER ]] ~~~
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
-vim.schedule( function()
+vim.schedule(function()
    vim.opt.clipboard = "unnamedplus"
-end )
+end)
 
 -- ~~~ [[ LOADING PLUGINS ]] ~~~
-require( "config.lazy" )
+require("config.lazy")
