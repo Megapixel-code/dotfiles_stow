a646e60dc60831bf17b9e5e2e1f7f8a4f386e4f3
Megapixel-code <chainemegapixel@gmail.com>
2025-12-25 00:17:48 +0100

    feat(nvim/snippets): added c function snippet

--- nvim/.config/nvim/plugin/snippets.lua
+++ nvim/.config/nvim/plugin/snippets.lua
@@ -1,9 +1,13 @@
 local ls = require("luasnip")
+local extras = require("luasnip.extras")
+local fmt = require("luasnip.extras.fmt")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
-local extras = require("luasnip.extras")
+local f = ls.function_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
 local rep = extras.rep
 
 vim.keymap.set({ "i", "s" }, "<A-n>", function()
@@ -19,6 +23,8 @@ vim.keymap.set({ "i", "s" }, "<A-p>", function()
 end, { silent = true, desc = "go to previous snippet jump" })
 
 
+
+-- --[[ GIT ]]--
 
 ls.add_snippets("gitcommit", {
    s("git_feat", {
@@ -66,6 +72,7 @@ ls.add_snippets("gitcommit", {
 })
 
 
+-- --[[ HTML ]]--
 
 ls.add_snippets("html", {
    s("tag", {
@@ -73,4 +80,65 @@ ls.add_snippets("html", {
       t({ "", "\t" }), i(0),
       t({ "", "</" }), rep(1), t(">"),
    })
+})
+
+
+local function docs_snip(args)
+   local insert = 1
+   local nodes = {
+      t("/**"),
+      t({ "", " * " }), i(1, "A short description"),
+   }
+   insert = insert + 1
+
+   t({ "", " * " })
+
+   for indx, arg in ipairs(vim.split(args[2][1], ", ")) do
+      local arg_type = vim.split(arg, " ")[1]
+      local arg_name = vim.split(arg, " ")[2]
+
+      if arg_type then
+         vim.list_extend(
+            nodes,
+            { t({ "", " * @param " .. arg_type }) }
+         )
+      end
+      if arg_name then
+         vim.list_extend(
+            nodes,
+            { t(" " .. arg_name .. " "), i(insert, "desc") }
+         )
+         insert = insert + 1
+      end
+   end
+
+   -- return
+   if args[1][1] ~= "void" then
+      vim.list_extend(
+         nodes,
+         { t({ "", " *" }), t({ " ", " * @return " }), i(insert) }
+      )
+      insert = insert + 1
+   end
+
+   -- add the end block
+   vim.list_extend(
+      nodes,
+      { t({ "", " */" }) }
+   )
+
+   return sn(nil, nodes)
+end
+
+-- --[[ C ]]--
+
+ls.add_snippets("c", {
+   s("fn", {
+      d(4, docs_snip, { 1, 3 }),
+      t({ "", "" }),
+      i(1, "type"), t(" "), i(2, "name"), t("("), i(3), t(")"),
+      t({ " {", "\t" }),
+      i(0),
+      t({ "", "}" }),
+   }),
 })
