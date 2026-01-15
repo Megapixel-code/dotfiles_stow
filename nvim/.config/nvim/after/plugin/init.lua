11de9047fbfdc786d8e71e00f64ad0711d683b5b
Megapixel-code <chainemegapixel@gmail.com>
2026-01-15 11:41:37 +0100

    feat(nvim/after): empty extensions are now sh filetype

--- nvim/.config/nvim/after/plugin/init.lua
+++ nvim/.config/nvim/after/plugin/init.lua
@@ -1,2 +1,9 @@
 -- this file is run after everything
 vim.diagnostic.config({ signs = false, virtual_text = true }) -- use virtual_text for diagnostics
+
+-- filetypes
+vim.filetype.add({
+   extension = {
+      [""] = "sh",
+   }
+})
