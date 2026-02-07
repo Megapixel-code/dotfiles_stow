e5f7bc04538ebfc7f78799873c50044eb8114afe
Megapixel-code <chainemegapixel@gmail.com>
2026-02-07 15:36:19 +0100

    feat(nvim/luasnip): updated the way the plugin is configured

--- nvim/.config/nvim/lua/plugins/utilities/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/utilities/luasnip.lua
@@ -1,7 +1,10 @@
 return {
    "L3MON4D3/LuaSnip",
-   -- follow latest release.
-   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-   -- install jsregexp (optional!).
+   -- version = "v2.*", -- enable this back if problems
+
    build = "make install_jsregexp",
+
+   opts = {
+      updateevents = "TextChanged,TextChangedI",
+   },
 }
