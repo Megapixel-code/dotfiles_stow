b19af6ac9d55c4960b452fecea3723c03b0f1ecd
Megapixel-code <chainemegapixel@gmail.com>
2026-02-16 13:08:50 +0100

    refactor(waybar/scripts): better way of writing bash

--- mangowc/.config/waybar/scripts/launch-waybar.sh
+++ mangowc/.config/waybar/scripts/launch-waybar.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
 
 trap "killall waybar" EXIT
 
-if [ "$(dpkg -l | grep -i "inotify-tools" | wc -l)" -lt 1 ];then
+if [ "$(dpkg -l | grep -i -c "inotify-tools")" -lt 1 ]; then
 	echo "you must install inotify-tools"
-	exit -1
+	exit 1
 fi
 
 while true; do
-    waybar &
-    inotifywait -e create,modify $CONFIG_FILES
-    killall waybar
+	waybar &
+	inotifywait -e create,modify "$CONFIG_FILES"
+	killall waybar
 done
--- mangowc/.config/waybar/scripts/open_setting.sh
+++ mangowc/.config/waybar/scripts/open_setting.sh
@@ -1,25 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-LAST_PID=`cat ~/.config/waybar/scripts/settings.pid`
-kill $LAST_PID $((LAST_PID+1))
+LAST_PID=$(cat ~/.config/waybar/scripts/settings.pid)
 
-if [ $? -ne 0 ]
-then 
-   sway mouse_warping none
+if [[ ! $( kill "$LAST_PID" $((LAST_PID + 1)))  ]]; then
+	sway mouse_warping none
 
-   MARGIN=130
+	MARGIN=130
 
-   `kitty $1` &
-   PID="$!"
-   echo "$PID" > ~/.config/waybar/scripts/settings.pid
+	kitty "$1" &
+	PID="$!"
+	echo "$PID" > ~/.config/waybar/scripts/settings.pid
 
-   sway for_window [pid="$((PID+1))"] floating enable
-   sway for_window [pid="$((PID+1))"] resize set width $((1920 - MARGIN))
-   sway for_window [pid="$((PID+1))"] resize set height $((1080 - MARGIN))
+	sway for_window [pid="$((PID + 1))"] floating enable
+	sway for_window [pid="$((PID + 1))"] resize set width $((1920 - MARGIN))
+	sway for_window [pid="$((PID + 1))"] resize set height $((1080 - MARGIN))
 
-   wait $PID
-   sway mouse_warping container
+	wait $PID
+	sway mouse_warping container
 fi
-
-
-
