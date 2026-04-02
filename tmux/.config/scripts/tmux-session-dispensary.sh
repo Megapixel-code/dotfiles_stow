a6499f175bae46c90abb1fd5ceb3a8ec046a62d4
megapixel-code <chainemegapixel@gmail.com>
2026-04-02 11:42:18 +0200

    feat(tmux/scripts): configured tmux-session-dispensary

--- tmux/.config/scripts/tmux-session-dispensary.sh
+++ tmux/.config/scripts/tmux-session-dispensary.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 # https://github.com/SylvanFranklin/.config/blob/main/scripts/tmux-session-dispensary.sh
 
+# colors:
+black=0
+red=1
+green=2
+yellow=3
+blue=4
+magenta=5
+cyan=6
+white=7
+bright_black=8
+bright_red=9
+bright_green=10
+bright_yellow=11
+bright_blue=12
+bright_magenta=13
+bright_cyan=14
+bright_white=15
+
+SK_THEME=(
+	--color=none,fg:"$white",hl:"$red",fg+:"$black",bg+:"$red",hl+:"$bright_white",current_match_bg:"$red",info:"$white",prompt:"$red",cursor:"$black"
+	--margin=25%
+	--prompt="λ "
+)
+
 DIRS=(
-   "$HOME/"
-   "$HOME/documents/"
-   "$HOME/documents/projects/"
+	"$HOME/"
+	"$HOME/documents/"
+	"$HOME/documents/projects/"
 )
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+	selected=$1
 else
-    selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=1 --full-path --base-directory "$HOME" \
-        | sed "s|^$HOME/||" \
-        | sk --margin 10% --color="bw")
+	selected=$(find "${DIRS[@]}" -maxdepth 1 -mindepth 1 -type d -not -path "*/.*" \
+		| sed "s|^$HOME/||" \
+		| sk "${SK_THEME[@]}")
 
-    [[ $selected ]] && selected="$HOME/$selected"
+	[[ $selected ]] && selected="$HOME/$selected"
 fi
 
 [[ ! $selected ]] && exit 0
@@ -22,8 +46,8 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 
 if ! tmux has-session -t "$selected_name"; then
-    tmux new-session -ds "$selected_name" -c "$selected"
-    tmux select-window -t "$selected_name:1"
+	tmux new-session -ds "$selected_name" -c "$selected"
+	tmux select-window -t "$selected_name:1"
 fi
 
 tmux switch-client -t "$selected_name"
